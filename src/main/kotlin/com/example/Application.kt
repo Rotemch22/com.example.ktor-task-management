f7cb024687b427c3968ead5b7ede38708c7ae565
rotemch <rotemch22@gmail.com>
2023-05-26 20:21:25 +0300

    Adding basic authentication for login

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -13,15 +13,20 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.auth.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.server.sessions.*
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.mindrot.jbcrypt.BCrypt
 import org.postgresql.ds.PGSimpleDataSource
 
+data class UserSession(val username: String) : Principal
 
 fun main() {
     val dataSource = PGSimpleDataSource().apply {
@@ -37,7 +42,7 @@ fun main() {
     val tasksRepository = TasksRepository(db)
 
     // Create the database tables
-    transaction (db) {
+    transaction(db) {
         SchemaUtils.create(UsersRepository.UsersTable, TasksRepository.TasksTable)
     }
 
@@ -75,12 +80,86 @@ fun Application.module(tasksService: TasksService, usersService: UsersService) {
         }
     }
 
-    routing {
-        taskRoutes(tasksService)
-        userRoutes(usersService)
+    install(Sessions) {
+        cookie<UserSession>("SESSION") {
+            cookie.extensions["SameSite"] = "lax" // Set the SameSite attribute for the cookie
+        }
     }
 
+
+    install(Authentication) {
+        session<UserSession>("sessionAuth") {
+            challenge("/login")
+            validate { session ->
+                // Validate the session and return the principal (user) associated with the session
+                UserSession(session.username)
+            }
+        }
+    }
+
+    routing {
+        route("/login") {
+            get {
+                // Serve the login form HTML
+                call.respondText(buildLoginForm(), ContentType.Text.Html)
+            }
+            post {
+                // Handle the form submission
+                val parameters = call.receiveParameters()
+                val username = parameters["username"]
+                val password = parameters["password"]
+
+                // Validate the username and password
+                val user = username?.let { usersService.getUserByUserName(username) }
+                if (user != null && BCrypt.checkpw(password, user.password)) {
+                    val session = UserSession(username)
+                    call.sessions.set(session)
+                    call.respondRedirect("/api") // Redirect to a default protected route
+
+                } else {
+                    call.respondRedirect("/login")
+                }
+            }
+        }
+
+        authenticate("sessionAuth") {
+            route("/api") {
+                // Handle the request to display available APIs
+                get {
+                    val apiList = listOf(
+                        "GET /tasks",
+                        "GET /tasks/{id}",
+                        "POST /tasks",
+                        "PUT /tasks/{id}",
+                        "DELETE /tasks/{id}",
+                        "GET /users",
+                        "GET /users/{id}",
+                        "POST /users",
+                        "PUT /users/{id}",
+                        "DELETE /users/{id}"
+                    )
+                    call.respondText(apiList.joinToString("\n"), ContentType.Text.Plain)
+                }
+            }
+            taskRoutes(tasksService)
+            userRoutes(usersService)
+        }
+    }
 }
 
 
-
+fun buildLoginForm(): String {
+    // Build and return the HTML form
+    return """
+        <html>
+        <body>
+            <h1>Login</h1>
+            <form method="POST" action="/login">
+                <input type="text" name="username" placeholder="Username"><br>
+                <input type="password" name="password" placeholder="Password"><br>
+                <button type="submit">Login</button>
+            </form>
+        </body>
+        </html>
+    """.trimIndent()
+}
